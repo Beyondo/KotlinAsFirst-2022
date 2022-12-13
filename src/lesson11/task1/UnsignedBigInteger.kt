fa60c28f98794df66b1b164f1489f70ad2723624
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-13 22:37:17 +0300

    Easiest task

--- src/lesson11/task1/UnsignedBigInteger.kt
+++ src/lesson11/task1/UnsignedBigInteger.kt
@@ -17,60 +17,153 @@ class UnsignedBigInteger : Comparable<UnsignedBigInteger> {
      * Конструктор из строки
      */
     constructor(s: String) {
-        TODO()
+        numberStr = s
     }
 
     /**
      * Конструктор из целого
      */
     constructor(i: Int) {
-        TODO()
+        numberStr = i.toString()
     }
 
     /**
      * Сложение
      */
-    operator fun plus(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun plus(other: UnsignedBigInteger): UnsignedBigInteger {
+        val first = numberStr.reversed()
+        val second = other.numberStr.reversed()
+        val result = StringBuilder()
+        var carry = 0
+        for (i in 0 until Math.max(first.length, second.length)) {
+            val firstDigit = if (i < first.length) first[i].toString().toInt() else 0
+            val secondDigit = if (i < second.length) second[i].toString().toInt() else 0
+            val sum = firstDigit + secondDigit + carry
+            result.append(sum % 10)
+            carry = sum / 10
+        }
+        if (carry > 0) result.append(carry)
+        return UnsignedBigInteger(result.toString().reversed())
+    }
 
     /**
      * Вычитание (бросить ArithmeticException, если this < other)
      */
-    operator fun minus(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun minus(other: UnsignedBigInteger): UnsignedBigInteger {
+        if (this < other) throw ArithmeticException()
+        val first = numberStr.reversed()
+        val second = other.numberStr.reversed()
+        val result = StringBuilder()
+        var carry = 0
+        for (i in 0 until first.length) {
+            val firstDigit = first[i].toString().toInt()
+            val secondDigit = if (i < second.length) second[i].toString().toInt() else 0
+            val diff = firstDigit - secondDigit - carry
+            if (diff < 0) {
+                result.append(diff + 10)
+                carry = 1
+            } else {
+                result.append(diff)
+                carry = 0
+            }
+        }
+        return UnsignedBigInteger(result.toString().reversed().trimStart('0'))
+    }
 
     /**
      * Умножение
      */
-    operator fun times(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun times(other: UnsignedBigInteger): UnsignedBigInteger {
+        val first = numberStr.reversed()
+        val second = other.numberStr.reversed()
+        val result = StringBuilder()
+        var carry = 0
+        for (i in 0 until first.length) {
+            val firstDigit = first[i].toString().toInt()
+            for (j in 0 until second.length) {
+                val secondDigit = second[j].toString().toInt()
+                val sum = firstDigit * secondDigit + carry
+                result.append(sum % 10)
+                carry = sum / 10
+            }
+            if (carry > 0) result.append(carry)
+            carry = 0
+        }
+        return UnsignedBigInteger(result.toString().reversed())
+    }
 
     /**
      * Деление
      */
-    operator fun div(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun div(other: UnsignedBigInteger): UnsignedBigInteger {
+        var first = numberStr.reversed()
+        val second = other.numberStr.reversed()
+        val result = StringBuilder()
+        var carry = 0
+        for (i in 0 until first.length) {
+            val firstDigit = first[i].toString().toInt()
+            val sum = firstDigit + carry * 10
+            result.append(sum / second.toInt())
+            carry = sum % second.toInt()
+        }
+        return UnsignedBigInteger(result.toString().reversed().trimStart('0'))
+    }
 
     /**
      * Взятие остатка
      */
-    operator fun rem(other: UnsignedBigInteger): UnsignedBigInteger = TODO()
+    operator fun rem(other: UnsignedBigInteger): UnsignedBigInteger {
+        var first = numberStr.reversed()
+        val second = other.numberStr.reversed()
+        val result = StringBuilder()
+        var carry = 0
+        for (i in 0 until first.length) {
+            val firstDigit = first[i].toString().toInt()
+            val sum = firstDigit + carry * 10
+            result.append(sum / second.toInt())
+            carry = sum % second.toInt()
+        }
+        return UnsignedBigInteger(carry.toString())
+    }
 
     /**
      * Сравнение на равенство (по контракту Any.equals)
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other is UnsignedBigInteger) {
+            return numberStr == other.numberStr
+        }
+        return false
+    }
 
     /**
      * Сравнение на больше/меньше (по контракту Comparable.compareTo)
      */
-    override fun compareTo(other: UnsignedBigInteger): Int = TODO()
+    override fun compareTo(other: UnsignedBigInteger): Int {
+        if (numberStr.length > other.numberStr.length) return 1
+        if (numberStr.length < other.numberStr.length) return -1
+        for (i in 0 until numberStr.length) {
+            if (numberStr[i] > other.numberStr[i]) return 1
+            if (numberStr[i] < other.numberStr[i]) return -1
+        }
+        return 0
+    }
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        return numberStr
+    }
 
     /**
      * Преобразование в целое
      * Если число не влезает в диапазон Int, бросить ArithmeticException
      */
-    fun toInt(): Int = TODO()
+    fun toInt(): Int {
+        if (numberStr.length > 10) throw ArithmeticException()
+        return numberStr.toInt()
+    }
 
+    private lateinit var numberStr: String
 }
