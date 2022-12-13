02670072122e5c80586dc02a9d911482011507b0
Beyondo <58893646+Beyondo@users.noreply.github.com>
2022-12-13 23:56:47 +0300

    Update TableFunction.kt

--- src/lesson12/task1/TableFunction.kt
+++ src/lesson12/task1/TableFunction.kt
@@ -2,6 +2,8 @@
 
 package lesson12.task1
 
+import kotlin.math.abs
+
 /**
  * Класс "табличная функция".
  *
@@ -18,7 +20,7 @@ class TableFunction {
     /**
      * Количество пар в таблице
      */
-    val size: Int get() = TODO()
+    val size: Int get() = table.size
 
     /**
      * Добавить новую пару.
@@ -26,7 +28,13 @@ class TableFunction {
      * или false, если она уже есть (в этом случае перезаписать значение y)
      */
     fun add(x: Double, y: Double): Boolean {
-        TODO()
+        return if (table.containsKey(x)) {
+            table[x] = y
+            false
+        } else {
+            table[x] = y
+            true
+        }
     }
 
     /**
@@ -34,20 +42,35 @@ class TableFunction {
      * Вернуть true, если пара была удалена.
      */
     fun remove(x: Double): Boolean {
-        TODO()
+        return if (table.containsKey(x)) {
+            table.remove(x)
+            true
+        } else {
+            false
+        }
     }
 
     /**
      * Вернуть коллекцию из всех пар в таблице
      */
-    fun getPairs(): Collection<Pair<Double, Double>> = TODO()
+    fun getPairs(): Collection<Pair<Double, Double>> = table.toList()
 
     /**
      * Вернуть пару, ближайшую к заданному x.
      * Если существует две ближайшие пары, вернуть пару с меньшим значением x.
      * Если таблица пуста, бросить IllegalStateException.
      */
-    fun findPair(x: Double): Pair<Double, Double>? = TODO()
+    fun findPair(x: Double): Pair<Double, Double>? {
+        var min = Double.MAX_VALUE
+        var result: Pair<Double, Double>? = null
+        for ((key, value) in table) {
+            if (abs(key - x) < min) {
+                min = abs(key - x)
+                result = key to value
+            }
+        }
+        return result
+    }
 
     /**
      * Вернуть значение y по заданному x.
@@ -57,11 +80,30 @@ class TableFunction {
      * Если существуют две пары, такие, что x1 < x < x2, использовать интерполяцию.
      * Если их нет, но существуют две пары, такие, что x1 < x2 < x или x > x2 > x1, использовать экстраполяцию.
      */
-    fun getValue(x: Double): Double = TODO()
+    fun getValue(x: Double): Double {
+        if (table.isEmpty()) throw IllegalStateException()
+        if (table.size == 1) return table.values.first()
+        val pair = findPair(x)
+        if (pair != null) return pair.second
+        val (x1, y1) = table.toList().sortedBy { it.first }.first { it.first > x }
+        val (x2, y2) = table.toList().sortedBy { it.first }.first { it.first < x }
+        return y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+    }
 
     /**
      * Таблицы равны, если в них одинаковое количество пар,
      * и любая пара из второй таблицы входит также и в первую
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other is TableFunction) {
+            if (table.size != other.table.size) return false
+            for ((key, value) in other.table) {
+                if (!table.containsKey(key) || table[key] != value) return false
+            }
+            return true
+        }
+        return false
+    }
+
+    private val table = HashMap<Double, Double>()
 }
